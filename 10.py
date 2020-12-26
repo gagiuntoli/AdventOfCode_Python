cb78c37bc5450c86cb6c8754871225ddafa98a40
Guido Giuntoli <gagiuntoli@gmail.com>
2020-12-26 12:33:35 +0100

    Add day 10 Conway Sequence

---
+++ 10.py
@@ -0,0 +1,29 @@
+# Conway Sequence Generator
+
+import fileinput
+
+def transform(num):
+	num_new = ""
+	i = 0
+	while i < len(num):
+		rep = 1
+		while i+rep < len(num):
+			if num[i] != num[i+rep]:
+				break
+			rep += 1
+		num_new += str(rep)+num[i]
+		i += rep
+	return num_new
+		
+		
+L = list([l.strip() for l in fileinput.input()])
+
+num = L[0]
+for i in range(40):
+	num = transform(num)
+print (len(num))
+
+num = L[0]
+for i in range(50):
+	num = transform(num)
+print (len(num))
